c3ada71ff16cb6bc31833145001444d2eb19780d
MuizSarwar <you@example.com>
2022-02-02 11:36:42 +0600

    This commit will end up the first class function

--- concept.js
+++ concept.js
@@ -1,5 +1,9 @@
 // 1. First class Function 
 
+// ****************Start ***************
+
+
+
 
  //what is an anonymous function ?(interview Q)
  //What are first class function ? (interview Q)
@@ -23,9 +27,11 @@
 
 
  //Anonymous Function : 
- function (){   }   // this is the way to create an anonymous function . 
+ //function (){   }   // this is the way to create an anonymous function . 
                     // But if we create or use  like this it will gives us an error .
+
                     //we can use it when we have to passed a function as a value .
+                    // we can also use it when we passed a function as an argument . 
 
 
 
@@ -36,6 +42,28 @@ const testFunction = function testIt(){ }  // this is a named function expressio
                                          //a function staement inside varibale . It will be a named functrion 
       // noticeable thing is that if we want to use 'testIt' Function outside the 'testFunction' it will gives us a syntext error . It is just because of SCOPE .                                   
 
+
+//Some example of First Class Function :
+ 
+//1. function uses as an argument of another function :
+function testOneFirstClass(parameterOne){
+    console.log(parameterOne) // this console will print function defination  
+    console.log(parameterOne()) // this console will call the function that is passed as argument 
+}
+
+
+testOneFirstClass(function(){return 'Hello';})
+
+
+
+//2. function returned from an function :
+function testTwoFirstClass(parameterTwo){
+    return function (){ 'returned function' };
+}
+
+console.log(testTwoFirstClass());
+
+//************************************END************************ */
 
 // 2. Higher Order Function 
 // 3. Reduce 
