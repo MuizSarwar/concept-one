8bb599897cab8587d1ed19318dac893ca1d29e1c
MuizSarwar <you@example.com>
2022-02-02 10:51:20 +0600

    learning first class function

---
+++ concept.js
@@ -0,0 +1,28 @@
+// 1. First class Function 
+
+
+ //what is an anonymous function ?(interview Q)
+ //What are first class function ? (interview Q)
+ // function statement vs expression vs declaration please explain it ? 
+
+ //Function statement :
+ function aFunction(){              // this is the way to create a function statement  
+                                    // function statement and declaretion are same 
+
+ }
+
+ //Function expression : 
+ const aFuncExpression = function (){       // If we assign an anonymous function inside a variable 
+                                            //this is the way to create a function expression 
+                                            // here function act like a value 
+ }
+
+ // so what is the difference between staement and expression ?? 
+ // answer : the diffference is in "Hoisting"  
+
+
+
+// 2. Higher Order Function 
+// 3. Reduce 
+// 4. Object loop 
+
